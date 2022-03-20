f4063eb0aabad7d7d40ed26ebaaa440bfef086b2
SHrustamovich <87068197+SHrustamovich@users.noreply.github.com>
2022-03-20 18:48:55 +0500

    three

--- src/model/model.sql
+++ src/model/model.sql
@@ -8,16 +8,16 @@ CREATE TABLE bank(
 );
   
 
-COPY (SELECT * FROM bank) TO 'C:\Users\User\Documents\bank\src\lib\file\visa.csv' CSV;
-\COPY bank FROM 'C:\Users\User\Documents\bank\src\lib\file\bf_visa.csv' DELIMITER ',' CSV HEADER;
+-- COPY (SELECT * FROM bank) TO 'C:\Users\User\Documents\bank\src\lib\file\visa.csv' CSV;
+-- \COPY bank FROM 'C:\Users\User\Documents\bank\src\lib\file\bf_visa.csv' DELIMITER ',' CSV HEADER;
 
-UPDATE
-   bank
-SET
-   branch = $1,
-    bank_customer_id =$2,
-    tieto_customer_id =$3,
-    head_customer_id =$4,
-    cur_acc= $5
-WHERE
-    id=$6
+-- UPDATE
+--    bank
+-- SET
+--    branch = $1,
+--     bank_customer_id =$2,
+--     tieto_customer_id =$3,
+--     head_customer_id =$4,
+--     cur_acc= $5
+-- WHERE
+--     id=$6
