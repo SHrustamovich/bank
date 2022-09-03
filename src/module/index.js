18344313a6023d4a11f24235a24154a5db42daf2
SHrustamovich <87068197+SHrustamovich@users.noreply.github.com>
2022-09-04 03:47:56 +0500

    d

--- src/module/index.js
+++ src/module/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const Bank = require('./bank/bank')
 router
 
- .get('/',Bank.GET_DATA)
+ .get('/get',Bank.GET_DATA)
  .post('/newdata',Bank.CREATE_DATA)
  .put('/updatedata',Bank.UPDATE_DATA)
  .delete('/deletedata',Bank.DELETE_DATA)
