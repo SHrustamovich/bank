bf05af1f7fc010f218f20ed0b6dec50aa34268f2
SHrustamovich <87068197+SHrustamovich@users.noreply.github.com>
2022-09-04 03:41:53 +0500

    co

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 const connection = {
-    connectionString: 'postgres://postgres:shohruh0949@localhost:5432/bank',
     connectionElString: 'postgres://gfehyluz:QKZYfuywpbJfcdGybzfSnAIF26ym-3bG@abul.db.elephantsql.com/gfehyluz'
 }
 const PORT = process.env.PORT || 9000
